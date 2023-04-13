bdcc02eed8a8eabbf7182dbb792ef8dda06cfa2c
Jakub Pomykała <jakub.pomykala@gmail.com>
2023-04-13 11:27:07 +0200

    Init

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import * as core from '@actions/core'
-import * as github from '@actions/github'
+// import * as github from '@actions/github'
 import * as exec from '@actions/exec'
 
 async function run(): Promise<void> {
@@ -29,8 +29,8 @@ async function run(): Promise<void> {
     const time = new Date().toTimeString()
     core.setOutput('time', time)
     // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`)
+    // const payload = JSON.stringify(github.context.payload, undefined, 2)
+    // console.log(`The event payload: ${payload}`)
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
